2774fcbc12dcc3636a5fa1eb6493183ccae755ce
Alamgir Ahamed Saif <36258593+alamgir72435@users.noreply.github.com>
2021-03-08 11:49:09 +0600

    convirt row date into date and get array between two days functions

--- index.js
+++ index.js
@@ -4,3 +4,19 @@ const textToNumber = (e) => {
     .replace(/[^0-9.]/g, "")
     .replace(/(\..*?)\..*/g, "$1");
 };
+
+// convirt input field Date into Date Object
+
+function convertIntoDate(date) {
+  var parts = date.split("-");
+  var myDate = new Date(parts[0], parts[1] - 1, parts[2]);
+  return myDate.toString() === "Invalid Date" ? null : myDate;
+}
+
+// Get array of Object between two Days
+function dataBetweenTwoDays(array, startDate, endDate) {
+  const newArray = array.filter((obj) => {
+    return obj.date >= startDate && obj.date <= endDate;
+  });
+  return newArray;
+}
