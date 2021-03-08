3c9dcd1bf85fb9c0b4330036662ed743a66967d3
Alamgir Ahamed Saif <36258593+alamgir72435@users.noreply.github.com>
2021-03-08 13:39:59 +0600

    number english to bangla

--- index.js
+++ index.js
@@ -5,6 +5,45 @@ const textToNumber = (e) => {
     .replace(/(\..*?)\..*/g, "$1");
 };
 
+// Number english to Bangla number
+function numEnToBn(num) {
+  let newNum = num.toString();
+  let numArray = newNum.split("");
+  let banglaNumber = "";
+  numArray[0] == "-" ? (banglaNumber += "-") : "";
+  numArray.forEach((each) => {
+    if (each == 1) {
+      banglaNumber += "১";
+    } else if (each == 2) {
+      banglaNumber += "২";
+    } else if (each == 3) {
+      banglaNumber += "৩";
+    } else if (each == 4) {
+      banglaNumber += "৪";
+    } else if (each == 5) {
+      banglaNumber += "৫";
+    } else if (each == 6) {
+      banglaNumber += "৬";
+    } else if (each == 7) {
+      banglaNumber += "৭";
+    } else if (each == 8) {
+      banglaNumber += "৮";
+    } else if (each == 9) {
+      banglaNumber += "৯";
+    } else if (each == 0) {
+      banglaNumber += "০";
+    } else if (each == ".") {
+      banglaNumber += ".";
+    } else if (each == ",") {
+      banglaNumber += ",";
+    } else if (each == "/") {
+      banglaNumber += "/";
+    }
+  });
+
+  return banglaNumber;
+}
+
 // convirt input field Date into Date Object
 
 function convertIntoDate(date) {
